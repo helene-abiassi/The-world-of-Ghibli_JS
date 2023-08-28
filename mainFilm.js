285eb8a23bd16eb990c4506ebed26b0080cac388
Helene Abi Assi <140400288+helene-abiassi@users.noreply.github.com>
2023-08-28 13:14:11 +0200

    Update mainFilm.js

--- mainFilm.js
+++ mainFilm.js
@@ -2,61 +2,47 @@ const getFilms = () => {
   const url = "https://ghibliapi.vercel.app/films";
   fetch(url)
     .then((response) => {
-      // console.log(response.json());
       return response.json();
     })
     .then((result) => {
-      // console.log("result :>> ", result);
       const ghibliFilms = result;
       postFilmCards(ghibliFilms);
+      sortEventListeners(ghibliFilms);
+      shuffleEventListener(ghibliFilms);
+      // return ghibliFilms;
     });
 };
 
 function postFilmCards(ghibliFilms) {
-  //   const cardsContainer =get document.getElementById("cards-container");
   const cardsContainer = document.getElementById("cards-container");
+  cardsContainer.innerHTML = "";
+  const modalDiv = document.createElement("div");
+
+  modalDiv.innerHTML = "";
 
   for (let i = 0; i < ghibliFilms.length; i++) {
-    // console.log("ghibliFilms :>> ", ghibliFilms[i]);
-
-    //CARD DIV
-
     const cardDiv = document.createElement("div");
     cardDiv.setAttribute("class", "card col-sm-12 col-md-6 col-lg-3");
-    cardDiv.setAttribute;
-
-    // console.log('cardDiv :>> ', cardDiv);
-
-    //CARD IMAGE
+    cardDiv.innerHTML = "";
 
     const image = document.createElement("img");
     image.setAttribute("src", ghibliFilms[i].image);
     image.setAttribute("alt", ghibliFilms[i].title);
     image.setAttribute("class", "card-image-top");
+    image.innerHTML = "";
     cardDiv.appendChild(image);
-
-    // console.log("image :>> ", image);
-
-    //CARD BODY
 
     const cardBody = document.createElement("div");
     cardBody.setAttribute("class", "card-body");
+    cardBody.innerHTML = "";
 
     cardsContainer.appendChild(cardDiv);
     cardDiv.appendChild(cardBody);
 
-    // console.log('cardBody :>> ', cardBody);
-
-    //CARD TITLE
-
     const h5 = document.createElement("h5");
-    h5.classList.add("card-title");
+    h5.setAttribute("class", "card-title");
     h5.innerText = ghibliFilms[i].title;
     cardBody.appendChild(h5);
-
-    // console.log('ghibliFilms[i].title :>> ', ghibliFilms[i].title);
-
-    //CARD TEXT
 
     const p = document.createElement("p");
     p.setAttribute("class", "card-text text-single-line");
@@ -69,56 +55,177 @@ function postFilmCards(ghibliFilms) {
     p2.innerText = ghibliFilms[i].director + ", " + ghibliFilms[i].release_date;
     cardBody.appendChild(p2);
 
-    // ADDITIONAL INFO
-
-    //CARD BUTTON TRIGGER MODAL
+    //CARD BUTTON
 
     const button = document.createElement("button");
+    button.setAttribute("type", "button");
     button.setAttribute("class", "btn btn-primary");
+    button.setAttribute("id", ghibliFilms[i].id);
     button.setAttribute("data-bs-toggle", "modal");
     button.setAttribute("data-bs-target", "#exampleModal");
 
     button.innerText = "Discover more";
-    cardBody.appendChild(button);
 
-    const modalDiv = document.createElement("div");
+    for (let j = 0; j < ghibliFilms.length; j++) {}
+
+    // MODAL TRIGGER
+    // const modalDiv = document.createElement("div");
     modalDiv.setAttribute("class", "modal fade");
     modalDiv.setAttribute("id", "exampleModal");
     modalDiv.setAttribute("tabindex", "-1");
     modalDiv.setAttribute("aria-labelledby", "exampleModalLabel");
     modalDiv.setAttribute("aria-hidden", "true");
-    cardBody.appendChild(modalDiv);
+    // modalDiv.innerHTML = ``;
 
     const modalDialog = document.createElement("div");
     modalDialog.setAttribute("class", "modal-dialog");
-    cardBody.appendChild(modalDialog);
+    modalDialog.setAttribute("id", "myModal");
 
     const modalContent = document.createElement("div");
-    modalContent.setAttribute("class", "modal-content");
-    cardBody.appendChild(modalContent);
+    modalContent.setAttribute("class", "modal-content card");
+    modalContent.setAttribute("id", "modal-content");
+    // modalContent.innerHTML = "";
+
+    modalDialog.appendChild(modalContent);
 
     const modalHeader = document.createElement("div");
     modalHeader.setAttribute("class", "modal-header");
+
+    modalContent.appendChild(modalHeader);
 
     const modalH1 = document.createElement("h1");
     modalH1.setAttribute("class", "modal-title fs-5");
     modalH1.setAttribute("id", "exampleModalLabel");
     modalH1.innerText = ghibliFilms[i].title;
+    const br = document.createElement("br");
 
-    const modalBtn = document.createElement("button");
-    modalBtn.setAttribute("type", "button");
-    modalBtn.setAttribute("class", "btn-close");
-    modalBtn.setAttribute("data-bs-dismiss", "modal");
-    modalBtn.setAttribute("aria-label", "Close");
+    const modalH1Jap = document.createElement("h2");
+    modalH1Jap.setAttribute("class", "modal-title fs-5");
+    modalH1Jap.setAttribute("id", "exampleModalLabel");
+    modalH1Jap.innerText = ghibliFilms[i].original_title + "\u00A0";
+
+    const modalButton = document.createElement("button");
+    modalButton.setAttribute("type", "button");
+    modalButton.setAttribute("class", "btn-close");
+    modalButton.setAttribute("data-bs-dismiss", "modal");
+    modalButton.setAttribute("aria-label", "Close");
+
     modalHeader.appendChild(modalH1);
-    modalHeader.appendChild(modalBtn);
+    modalH1.appendChild(br);
 
-    // modalDialog.appendChild(modalHeader);
+    modalH1.appendChild(modalH1Jap);
+
+    modalHeader.appendChild(modalButton);
+
+    const modalBody = document.createElement("div");
+    modalBody.setAttribute("class", "modal-body");
+    modalBody.innerHTML = "";
+
+    const modalBanner = document.createElement("img");
+    modalBanner.setAttribute("src", ghibliFilms[i].movie_banner);
+    modalBanner.setAttribute("class", "modal-body");
+    modalBanner.setAttribute("alt", ghibliFilms[i].title);
+    modalBanner.setAttribute(
+      "style",
+      "width: 100%; border-color: #4ba762; border-style: solid; padding: 0px"
+    );
+    modalBanner.setAttribute("alt", ghibliFilms[i].id);
+    modalBanner.innerHTML = "";
+
+    const modalDirector = document.createElement("h6");
+    modalDirector.setAttribute("style", "padding-top: 10px");
+    modalDirector.innerText =
+      ghibliFilms[i].director + "," + "\u00A0" + ghibliFilms[i].release_date;
+
+    const modalRating = document.createElement("h6");
+    modalRating.innerText =
+      "Rating:" + "\u00A0" + ghibliFilms[i].rt_score + "%";
+
+    const modalText = document.createElement("p");
+    modalText.innerText = ghibliFilms[i].description;
+
+    const modalRunTime = document.createElement("h6");
+    modalRunTime.innerText =
+      "Running time:" +
+      "\u00A0" +
+      ghibliFilms[i].running_time +
+      "\u00A0" +
+      "mns";
+
+    modalBody.appendChild(modalBanner);
+    modalBody.appendChild(modalDirector);
+    modalBody.appendChild(modalRating);
+    modalBody.appendChild(modalText);
+    modalBody.appendChild(modalRunTime);
+
+    cardBody.appendChild(button);
+    cardBody.appendChild(modalDiv);
+
+    modalContent.appendChild(modalBody);
+    modalDiv.appendChild(modalDialog);
   }
 }
 
+const sortEventListeners = (ghibliFilms) => {
+  const filmSort = document.getElementById("filmSortBy");
+
+  filmSort.addEventListener("change", (e) => {
+    console.log(e.target.value);
+    sortByDropDown(ghibliFilms);
+  });
+};
+
+const sortByDropDown = (ghibliFilms) => {
+  const sortedFilms = [...ghibliFilms];
+  // console.log(sortedFilms);
+
+  const filmSort = document.getElementById("filmSortBy");
+  let filmSortValue = filmSort.value;
+
+  if (filmSortValue === "default") {
+    postFilmCards(ghibliFilms);
+  } else if (filmSortValue === "A to Z") {
+    sortedFilms.sort((a, b) => a.title.localeCompare(b.title));
+    postFilmCards(sortedFilms);
+  } else if (filmSortValue === "Release Year") {
+    sortedFilms.sort((a, b) => b.release_date - a.release_date);
+    postFilmCards(sortedFilms);
+  } else if (filmSortValue === "Rating") {
+    sortedFilms.sort((a, b) => b.rt_score - a.rt_score);
+    postFilmCards(sortedFilms);
+  } else if (filmSortValue === "Running Time")
+    sortedFilms.sort((a, b) => b.running_time - a.running_time);
+  postFilmCards(sortedFilms);
+};
+
+const shuffleEventListener = (ghibliFilms) => {
+  const filmShuffle = document.getElementById("shuffleFilmButton");
+
+  filmShuffle.addEventListener("click", (e) => {
+    console.log(e.target.value);
+    shuffleButton(ghibliFilms);
+  });
+};
+
+const shuffleButton = (ghibliFilms) => {
+  const sortedFilms = [...ghibliFilms];
+
+  const filmShuffle = document.getElementById("shuffleFilmButton");
+  let shuffleSortValue = filmShuffle.value;
+
+  if (shuffleSortValue === "Shuffle") {
+    sortedFilms.sort(() => Math.random() - 0.5);
+    // console.log(sortedFilms);
+    postFilmCards(sortedFilms);
+  } else {
+    postFilmCards(ghibliFilms);
+  }
+};
+
 getFilms();
 
-// postFilmCards(getFilms);
-
-// getChFilms();
+document
+  .getElementById("learnMoreTextChange")
+  .addEventListener("click", function () {
+    this.innerText = "Learn Less";
+  });
